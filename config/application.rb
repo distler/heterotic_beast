b067d843a22c07c186512d6941dc4ef4d3bd53d2
Jacques Distler <distler@golem.ph.utexas.edu>
2011-09-02 07:56:46 -0500

    Default Configuration Options for Asset Pipeline

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,11 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module AlteredBeast
   class Application < Rails::Application
+
     config.assets.enabled = true
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
