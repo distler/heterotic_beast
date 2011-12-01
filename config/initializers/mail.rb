8d7fc00772500279cdd5ded0c89c2ca12397b19b
Jacques Distler <distler@golem.ph.utexas.edu>
2011-11-30 20:13:48 -0600

    More sensible defaults
    With a local SMTP server,
    these defaults will typically work
    out-of-the-box.

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -3,11 +3,11 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
  :address               => "localhost",
  :port                  => 25,
- :domain                => "mydomain.com",
- :user_name             => "mailbox_user",
- :password              => "mailbox_password",
- :authentication        => :cram_md5, # or :plain or :login
- :enable_starttls_auto => true
+# :domain                => "mydomain.com",
+# :user_name             => "mailbox_user",
+# :password              => "mailbox_password",
+# :authentication        => :cram_md5, # or :plain or :login
+ :enable_starttls_auto => false
 }
 ##Configuration for using sendmail
 #ActionMailer::Base.delivery_method = :sendmail
