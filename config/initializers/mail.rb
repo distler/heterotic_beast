0bf31c8ea6d7ebfc85552a2b9b6ed518d9344039
Jacques Distler <distler@golem.ph.utexas.edu>
2011-12-03 11:31:55 -0600

    Clean that up, a bit more

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,5 +1,7 @@
+##Choose the method for delivering signup emails
+ActionMailer::Base.delivery_method = :smtp # or :sendmail
+
 ##Configuration for using an SMTP server 
-ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
  :address               => "localhost",
  :port                  => 25,
@@ -7,11 +9,11 @@ ActionMailer::Base.smtp_settings = {
 # :user_name             => "mailbox_user",
 # :password              => "mailbox_password",
 # :authentication        => :cram_md5, # or :plain or :login
- :enable_starttls_auto => false
+ :enable_starttls_auto  => false
 }
+
 ##Configuration for using sendmail
-#ActionMailer::Base.delivery_method = :sendmail
-#ActionMailer::Base.sendmail_settings = {
-#  :location => '/usr/sbin/sendmail',
-#  :arguments => '-i -t'
-#}
+ActionMailer::Base.sendmail_settings = {
+  :location   => '/usr/sbin/sendmail',
+  :arguments  => '-i -t'
+}
