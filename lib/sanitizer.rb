3415af25721f971e1cb22e41b40209c4f1ed3ef3
Jacques Distler <distler@golem.ph.utexas.edu>
2026-06-05 21:31:42 -0500

    Tighten up sanitization of SVG attributes

--- lib/sanitizer.rb
+++ lib/sanitizer.rb
@@ -181,6 +181,11 @@ module Sanitizer
         if attr == 'xlink:href' && SVG_ALLOW_LOCAL_HREF.include?(node.name) && val =~ /^\s*[^#\s]/m
           node.attributes.delete attr; next
         end
+        if attr == 'attributeName' && ATTR_VAL_IS_URI.include?(val)
+          node.attributes.delete attr
+          node.attributes.delete 'values' if node.attributes['values']
+          next
+        end
         if ATTR_VAL_IS_URI.include?(attr)
           val_unescaped = val.unescapeHTML.as_bytes.gsub(/`|[\000-\040\177\s]+|\302[\200-\240]/n,'').downcase
           if val_unescaped =~ /^[a-z0-9][-+.a-z0-9]*:/ && !ALLOWED_PROTOCOLS.include?(val_unescaped.split(':')[0]) 
@@ -229,11 +234,13 @@ module Sanitizer
 # The string returned is utf-8 encoded. If you want, you can use iconv to convert it to some other encoding.
 # (REXML trees are always utf-8 encoded.)
   def safe_xhtml_sanitize(html, options = {})
-    sanitized = xhtml_sanitize(html.purify)
-    doc = Nokogiri::XML::Document.parse("<div xmlns='http://www.w3.org/1999/xhtml'>#{sanitized}</div>", nil, (options[:encoding] || 'UTF-8'), 0)
-    sanitized = doc.root.children.to_xml(:indent => (options[:indent] || 2), :save_with => 2 )
+    begin
+      sanitized = xhtml_sanitize(html.purify)
+      doc = Nokogiri::XML::Document.parse("<div xmlns='http://www.w3.org/1999/xhtml'>#{sanitized}</div>", nil, (options[:encoding] || 'UTF-8'), 0)
+      sanitized = doc.root.children.to_xml(:indent => (options[:indent] || 2), :save_with => 2 )
     rescue Nokogiri::XML::SyntaxError
       sanitized = sanitized.escapeHTML
+    end
   end
 
 end
